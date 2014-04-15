b89f2615e485d5f8263e295cb8e4c536c0f7ca03
Corentin Noël <tintou@mailoo.org>
2014-04-16 00:25:11 +0200

    Added missing files into core.

--- core/maya-calendar-1.pc.cmake
+++ core/maya-calendar-1.pc.cmake
@@ -8,4 +8,4 @@ Description: Maya headers
 Version: 0.2  
 Libs: -lmaya-calendar
 Cflags: -I@DOLLAR@{includedir}/maya-calendar
-Requires: glib-2.0 gio-2.0 gee-0.8 libpeas-1.0 gtk+-3.0 granite libecalendar-1.2 libedataserver-1.2 libical
+Requires: glib-2.0 gio-2.0 gee-0.8 gmodule-2.0 gtk+-3.0 granite libecalendar-1.2 libedataserver-1.2 libical
