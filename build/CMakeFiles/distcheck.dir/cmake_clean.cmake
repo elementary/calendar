d747d8fe4cd0f78051700fba9c53497d69a0dcb9
Phillip Sz <>
2014-08-10 22:07:31 +0200

    remove build

--- build/CMakeFiles/distcheck.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/distcheck"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/distcheck.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
